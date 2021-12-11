01a14a8ec052613cb0fd667c110a31b8f16b4bbf
smg <smg38@yandex.ru>
2021-12-11 21:28:22 +0300

    fix: color text

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 #set -x
 COLOR='\e[42m'
-NC='\e[49m' # No Color
+RED='\e[1;31m'
+DEF=
+NC='\e[m'       #'\e[49m' # No Color
+PORT=88
 
 tag="test"
 build_args="--no-cache"
@@ -11,7 +14,9 @@ printf "${COLOR}Build container${NC}\n"
 docker build --file=Dockerfile ${build_args} --tag=${tag} .
 
 printf "${COLOR}Start container${NC}\n"
-docker run -p 88:88 --name my-${tag} ${tag}
+printf "${COLOR}Web server URL http://localhost:${PORT}${NC}\n"
+printf "${COLOR}${NC}${RED}CTRL+C${NC}${COLOR} - to stop container${NC}"
+docker run -p ${PORT}:${PORT} --name my-${tag} ${tag}
 
 printf "${COLOR}Delete container${NC}\n"
 docker rm my-${tag}
